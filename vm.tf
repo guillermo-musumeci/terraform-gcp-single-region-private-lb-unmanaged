41872fcdd740c12841f8c0aac7310ebad4443198
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-11-12 13:17:57 +0100

    Initial commit

--- vm.tf
+++ vm.tf
@@ -43,20 +43,3 @@ resource "google_compute_instance" "web_private_2" {
     subnetwork    = google_compute_subnetwork.private_subnet_1.name
   }
 } 
-
-# Virtual machine output | vm-output.tf
-output "web-1-name" {
-  value = google_compute_instance.web_private_1.name
-}
-
-output "web-1-internal-ip" {
-  value = google_compute_instance.web_private_1.network_interface.0.network_ip
-}
-
-output "web-2-name" {
-  value = google_compute_instance.web_private_2.name
-}
-
-output "web-2-internal" {
-  value = google_compute_instance.web_private_2.network_interface.0.network_ip
-}
